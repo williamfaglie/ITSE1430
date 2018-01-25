1fdf0463d56dc455173f53130271e44536862d92
williamfaglie <william.faglie408@my.tccd.edu>
2018-01-24 21:43:20 -0600

    Nile.Host 1/24/18

---
+++ Classwork/Section1/Nile.Host/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+
+namespace Nile.Host
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            
+        }
+        static void PlayingWithPrimitives ()
+        {
+            //Primitive
+            decimal unitPrice = 10.5M;
+
+            //Real declaration (Framework)
+            System.Decimal unitPrice2 = 10.5M;
+
+            //Current time (Must use Framework)
+            System.DateTime now = DateTime.Now;
+            DateTime now1 = DateTime.Now;
+
+            System.Collections.ArrayList items;
+        }
+
+        static void PlayingWithVariables ()
+        {
+            int hours = 0;
+
+            double rate = 10.25;
+
+            //Still not assigned
+            //if (false)
+            //    hours = 0;
+
+            int hours2 = hours;
+
+            string firstName, lastName;
+
+            //string @class;
+
+            //Single assignment
+            firstName = "Bob";
+            lastName = "Miller";
+
+            //Multiple assignment
+            firstName = lastName = "Sue";
+
+            //Math ops
+            int x = 0, y = 10;
+            int add = x + y;
+            int subtract = x - y;
+            int mulitply = x * y;
+            int divide = x / y;
+            int modulos = x % y;
+
+            //x = x + 10;
+            x += 10;
+            double ceiling = Math.Ceiling(rate);
+            double floor = ceiling;
+        }
+    }
+}
