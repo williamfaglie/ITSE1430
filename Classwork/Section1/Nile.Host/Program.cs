26c3e8a3966ae011d330cc90e1e2cfd2d1f174e8
williamfaglie <william.faglie408@my.tccd.edu>
2018-01-29 21:39:06 -0600

    1/29/18 Lecture

--- Classwork/Section1/Nile.Host/Program.cs
+++ Classwork/Section1/Nile.Host/Program.cs
@@ -7,8 +7,113 @@ namespace Nile.Host
     {
         static void Main( string[] args )
         {
-            
+           bool quit = false;
+           while (!quit)
+            {
+                //Display menu
+                char choice = DisplayMenu();
+
+                //Process menu selection
+                switch (choice)
+                {
+                    //case 'l':
+                    case 'L': ListProducts(); break;
+
+                    //case 'a':
+                    case 'A': AddProduct(); break;
+
+                    //case 'q':
+                    case 'Q': quit = true; break;
+                };
+            };
         }
+
+        static void AddProduct ()
+        {
+            //Get name
+            _name = ReadString("Enter name: ", true);
+
+            //Get price
+            _price = ReadDecimal("Enter price: ", 0);
+
+            //Get description
+            _description = ReadString("Enter description: ", false);
+        }
+
+        private static decimal ReadDecimal( string message, decimal minValue )
+        {
+            do
+            {
+                Console.Write(message);
+
+                string value = Console.ReadLine();
+
+                if (Decimal.TryParse(value, out decimal result))
+                {
+                    //If not required or not empty
+                    if (result >= minValue)
+                        return result;
+                };
+
+                Console.WriteLine("Value must be >= {0}", minValue);
+            } while (true);
+        }
+
+        private static string ReadString( string message, bool isRequired)
+        {
+            do
+            {
+                Console.Write(message);
+
+                string value = Console.ReadLine();
+
+                //If not required or not empty
+                if (!isRequired || value != "")
+                    return value;
+
+                Console.WriteLine("Value is required");
+            } while (true);
+        }
+
+        private static char DisplayMenu()
+        {
+            do
+            {
+                Console.WriteLine("L)ist products");
+                Console.WriteLine("A)dd product");
+                Console.WriteLine("Q)uit");
+
+                string input = Console.ReadLine();
+
+                if (input == "L")
+                    return input[0];
+                else if (input == "A")
+                    return input[0];
+                else if (input == "Q")
+                    return input[0];
+                
+                Console.WriteLine("PLease choose a valid option");
+            } while (true);
+        }
+
+        static void ListProducts()
+        {
+            // Are there any products?
+            if (_name != null && _name != "")
+            {
+                // Display a product
+                Console.WriteLine(_name);
+                Console.WriteLine(_price);
+                Console.WriteLine(_description);
+            } else
+                Console.WriteLine("No products");
+        }
+
+        //Data for a product
+        static string _name;
+        static decimal _price;
+        static string _description;
+
         static void PlayingWithPrimitives ()
         {
             //Primitive
