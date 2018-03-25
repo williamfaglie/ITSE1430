15f8fa76d3451368d077519e7a7138deb6e7f155
williamfaglie <william.faglie408@my.tccd.edu>
2018-03-25 16:10:01 -0500

    3/25/18 Lab3 4:00 pm

---
+++ Labs/Lab3/WilliamFaglie.MovieLib/Data/IMovieDatabase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WilliamFaglie.MovieLib.Data
+{
+    class IMovieDatabase
+    {
+        Product Add( Product product, out string message );
+        Product Update( Product product, out string message );
+        IEnumerable<Product> GetAll();
+        void Remove( int id );
+    }
+}
